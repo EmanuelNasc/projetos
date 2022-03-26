4f387fe6da845d92d4e017e7dacffad3a8386b84
Realsolucoesbr <91483560+Realsolucoesbr@users.noreply.github.com>
2022-03-26 19:41:56 -0300

    novo jogo
    jogo sendo criado para demonstração de domínio de alguns aspectos de integração js e a pagina html

---
+++ escolhas.js
@@ -0,0 +1,53 @@
+'criar um sistema em js, jogo, para fazer duas ou tres escolhas
+com 4 etapas
+para alcançar a linha de chegada'
+
+começa do começo mesmo
+
+1a escolha: sair de carro, ou a pé dois botões
+
+se sair de carro, alcança um sinal e se atrasará pra reuniao
+
+
+se sair a pe 
+
+é assaltado, volta do inicio, não existe mais opção de escolher ir a pé
+
+
+
+se escolher sair de carro
+
+depois de parar no sinal, um setrandom para dizer se vai ser parado ou não por um policial tipo
+
+setrandom(1,2)
+if 1 ("policial para o carro")
+else ("policial olha pra você e abre caminho, você não chegará atrasado")
+
+próximo ponto é chegar na empresa
+
+"você chega na empresa com 10 minutos pra se preparar pra reunião
+e vê uma grande fila pra pegar o elevador pro escritório e tem um grupo de escadas de emergência, o que vc escolhe?"
+
+se escolher pegar as escadas, aparece um segurança novato no 14o andar que o faz voltar para a recepção para averiguação
+
+se escolher pegar o elevador, aparece um chefe que o cumprimenta e ambos conversam sobre o transito e a reuniao
+
+se a pessoa tiver escolhido pegar as escadas, ela volta ao ponto de decisao novamente mas a fila estara maior, se escolher pegar as escadas novamente se atrasará 10 minutos, se pegar o elevador vai ver o chefe subindo e ela vai chegar 2 minutos atrasado
+
+
+ultima decisao
+
+se a pessoa veio de elevador na primeira decisão, chega 2 minutos atrasados, mas o chefe lhe ajuda pois o mesmo viu que chegou no mesmo horario que ele
+
+fim de jogo nessa opção, aparece "bom trabalho, conseguiu uma promoção, apresentação concluída."
+
+se a pessoa subiu as escadas e teve que escolher qualquer dos ambos caminhos possíveis aparece mais um diálogo:
+"os chefes chegaram e todos estão insatisfeitos por você não ter chegado cedo, o que você faz?"
+
+aparece uma zona de opção: mentir ou falar a verdade
+
+se a pessoa mentir, o segurança aparece e desmente, ele é demitido
+
+se a pessoa falar a verdade, aparece: tudo bem, seja mais prudente
+
+fim de jogo
